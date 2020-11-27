c2bdd72ca52525afe204e84564420e3e36f684ea
Karuna Sato <karutimes2@comcast.net>
2020-11-27 00:51:04 -0800

    connected db to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hskbfcoronkx7ktz',
-    database: 'burger_db'
-  });
-};
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.sqlpw,
+  database: "burgers_db"
+});
 
-connection.connect();
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
